49f82a78ec1b7de6da39213c67b43a07fe9bb223
sandeep <sanme2@gmail.com>
2024-02-25 09:41:44 +0000

    Updated on 25 feb

--- PMPizza.py
+++ PMPizza.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 
-PMPizza = Flask(__name__)
+Pizza = Flask(__name__)
 
-@PMPizza.route('/')
+@Pizza.route('/')
 def index():
     return render_template('pizzahome.html')
 
 if __name__ == '__main__':
-    PMPizza.run(debug=True)
+    Pizza.run(debug=True)
